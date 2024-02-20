f6e7169886a6f341a9cdffad59570b005f05c6b4
Samantha Hirsch <sammyhirsch0109@gmail.com>
2024-02-20 13:47:26 -0500

    fixed name thing

--- script.js
+++ script.js
@@ -7,14 +7,65 @@ let madLibOutputDiv = document.getElementById('nameChange')
 
 nameChange.innerHTML = `<p>${story}</p>`
 
-let playerScore = 0;
-let computerScore = 0;
-let rounds = 0;
-const maxRounds = 6;
-const roundResults = []; 
 
-const playerScoreDisplay = document.getElementById("playerScoreDisplay");
-const computerScoreDisplay = document.getElementById("computerScoreDisplay");
-const playerChoiceDisplay = document.getElementById("playerChoice");
-const computerChoiceDisplay = document.getElementById("computerChoice");
-const roundNumberDisplay = document.getElementById("roundNumber");
+
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    if (userInput === "rock" || userInput === "paper" || userInput === "scissors" || userInput ==="bomb") {
+      return userInput;
+    } else {
+      console.log("Error!");
+    }
+  }
+  
+  const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+      case 0:
+        return "rock";
+      case 1:
+        return "paper";
+      case 2:
+        return "scissors";
+    }
+  }
+  
+  const determineWinner = (userChoice,computerChoice) => {
+    if (userChoice === "bomb") {
+      return "you are unbeatable winner"
+    };
+    if (userChoice === computerChoice){
+      return "That is a tie!";
+    };
+    if (userChoice === "rock") {
+      if (computerChoice === "paper") {
+         return "Computer won";
+    } else {
+      return "User won";
+    }
+    }
+    if (userChoice === "paper") {
+      if (computerChoice === "scissors") {
+         return "Computer won";
+    } else {
+      return "User won";
+    }
+    }
+    if (userChoice === "scissors") {
+      if (computerChoice === "rock") {
+         return "Computer won";
+    } else {
+      return "User won";
+    }
+    }
+  }
+  
+  const playGame = () => {
+    console.log(`player chose ${getPlayerChoice('scissors')}`);
+    console.log(`computer chose ${getComputerChoice()}`);
+    console.log(determineWinner(getUserChoice("scissors"),getComputerChoice()));
+  }
+  playGame();
+    
+  
+  
