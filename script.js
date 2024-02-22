661fe34c4cce4b69f8ec88c9d84df1fbec727c42
Samantha Hirsch <sammyhirsch0109@gmail.com>
2024-02-22 14:50:47 -0500

    some javascript

--- script.js
+++ script.js
@@ -5,18 +5,21 @@ let story = ` Player Name: <b> ${name} </b>`
 
 nameChange.innerHTML = `<p>${story}</p>`
 
-  function playGame(userSelection) {
-  const randomNumber = Math.floor(Math.random()*3);
-   if (randomNumber === 1) {
-    computerChoice = 'rock'
-   }
-   else if ( randomNumber === 2){
-    computerChoice = 'paper'
-   }
-  else (randomNumber === 3){
-    computerChoice = 'scissors'
-  }
 
-  document.getElementById('computerChoice').textContent = computerChoice
-  document.getElementById('playerChoice').textContent = playerChoice
-  }
+function playRound(playerChoice){
+ //Get Computer Choice
+ const randomNumber = Math.floor(Math.random()*3);
+ if (randomNumber === 1) {
+     computerChoice = 'Rock'
+ }
+ else if (randomNumber === 2) {
+     computerChoice = 'Paper'
+ }
+ else  {
+     computerChoice = 'Scissor'
+ }
+//Display Selections
+
+const playerChoice = document.getElementById("playerChoice");
+const computerChoice = document.getElementById("computerChoice");
+}
