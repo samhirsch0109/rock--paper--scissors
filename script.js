9d146e52aa588fa60784511a3bd69916e064cb93
samhirsch0109 <144719425+samhirsch0109@users.noreply.github.com>
2024-02-26 17:42:39 +0000

    js works

--- script.js
+++ script.js
@@ -6,20 +6,23 @@ let story = ` Player Name: <b> ${name} </b>`
 nameChange.innerHTML = `<p>${story}</p>`
 
 
-function playRound(playerChoice){
- //Get Computer Choice
- const randomNumber = Math.floor(Math.random()*3);
- if (randomNumber === 1) {
-     computerChoice = 'Rock'
- }
- else if (randomNumber === 2) {
-     computerChoice = 'Paper'
- }
- else  {
-     computerChoice = 'Scissor'
- }
-//Display Selections
+function playGame(playerChoice) {
+    // Get Computer Choice
+    const randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice;
+    if (randomNumber === 0) {
+        computerChoice = 'Rock';
+    } else if (randomNumber === 1) {
+        computerChoice = 'Paper';
+    } else {
+        computerChoice = 'Scissor';
+    }
 
-const playerChoice = document.getElementById("playerChoice");
-const computerChoice = document.getElementById("computerChoice");
-} 
+
+    playerChoice = playerChoice.toUpperCase();
+    computerChoice = computerChoice.toUpperCase();
+
+    // Display Selections
+    document.getElementById("playerChoice").innerHTML = `<h5 class="text-center">Player Choice: ${playerChoice}</h5>`;
+    document.getElementById("computerChoice").innerHTML = `<h5 class="text-center">Computer Choice: ${computerChoice}</h5>`;
+}
